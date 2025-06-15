a7447203ecc4ae813e27bf2147ee9d5706c121c5
SumitTapa <164578930+SumitTapa@users.noreply.github.com>
2025-06-16 00:31:43 +0530

    Add files via upload

---
+++ math_operations.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"Addition: {num1 + num2}\n")
+print(f"Subtraction: {num1 - num2}\n")
+print(f"Multiplication: {num1 * num2}\n")
+print(f"Division: {num1 / num2}")
